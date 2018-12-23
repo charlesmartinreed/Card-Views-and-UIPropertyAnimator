1a15d067ae67959cf1ca656cd7a0e00aae710b0a
Charles Martin Reed <charlesmartinreed@icloud.com>
2018-12-22 23:01:53 -0600

    Add height and corner radius cardViewController animations. Add viewController blur animation.

--- Card Views and UIPropertyAnimator/ViewController.swift
+++ Card Views and UIPropertyAnimator/ViewController.swift
@@ -51,9 +51,122 @@ class ViewController: UIViewController {
         //set cardViewController frame using the variables instanced above
         cardViewController.view.frame = CGRect(x: 0, y: self.view.frame.height - cardHandleAreaHeight, width: self.view.bounds.width, height: cardHeight)
         cardViewController.view.clipsToBounds = true
+        
+        //define gesture recognizer
+        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(handleCardTap(_:)))
+        let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handleCardPan(_:)))
+        
+        cardViewController.handleArea.addGestureRecognizer(tapGestureRecognizer)
+        cardViewController.handleArea.addGestureRecognizer(panGestureRecognizer)
     }
     
 
+    //MARK:- Gesture handling
+    @objc func handleCardTap(_ recognizer: UITapGestureRecognizer) {
+        
+    }
+    
+    @objc func handleCardPan(_ recognizer: UIPanGestureRecognizer) {
+        //pan gesture recognizer has multiple states
+        switch recognizer.state {
+        case .began:
+            //placed finger
+            startInteractiveTransition(state: nextState, duration: 0.9)
+        case .changed:
+            //moved finger
+            let translation = recognizer.translation(in: self.cardViewController.handleArea)
+            var fractionComplete = translation.y / cardHeight
+            fractionComplete = isCardVisible ? fractionComplete : -fractionComplete
+            updateInteractiveTransition(fractionCompleted: fractionComplete)
+        case .ended:
+            //lifted finger
+            continueInteractiveTransition()
+        default:
+            break
+        }
+    }
+    
+    //MARK:- Animation creation
+    func animateTransationIfNeeded(state: CardState, duration: TimeInterval) {
+        //called if animation is needed, like if runningAnimation arraya is empty
+        
+        if runningAnimations.isEmpty {
+            //animating card height for cardViewController
+            let frameAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                //move card view up or down
+                switch state {
+                case .expanded:
+                    self.cardViewController.view.frame.origin.y = self.view.frame.height - self.cardHeight
+                case .collapsed:
+                    self.cardViewController.view.frame.origin.y = self.view.frame.height - self.cardHandleAreaHeight
+                }
+            }
+            
+            //if card has been moved all the way up or down...
+            frameAnimator.addCompletion { (_) in
+                self.isCardVisible = !self.isCardVisible
+                self.runningAnimations.removeAll()
+            }
+            
+            frameAnimator.startAnimation()
+            runningAnimations.append(frameAnimator)
+            
+            //animating corner radius for cardViewController
+            let cornerRadiusAnimator = UIViewPropertyAnimator(duration: duration, curve: .linear) {
+                switch state {
+                case .expanded:
+                    self.cardViewController.view.layer.cornerRadius = 12
+                case .collapsed:
+                    self.cardViewController.view.layer.cornerRadius = 0
+                }
+            }
+            
+            cornerRadiusAnimator.startAnimation()
+            runningAnimations.append(cornerRadiusAnimator)
+            
+            //animating blur for view controller
+            let blurAnimator = UIViewPropertyAnimator(duration: duration, dampingRatio: 1) {
+                switch state {
+                case .expanded:
+                    self.visualEffectView.effect = UIBlurEffect(style: .dark)
+                case .collapsed:
+                    self.visualEffectView.effect = nil
+                }
+            }
+            
+            blurAnimator.startAnimation()
+            runningAnimations.append(blurAnimator)
+        }
+    }
+    
+    //MARK:- Animation state handling
+    func startInteractiveTransition(state: CardState, duration:TimeInterval) {
+        //check if we currently have animations
+        if runningAnimations.isEmpty {
+            //no? Then, run animations.
+            animateTransationIfNeeded(state: state, duration: duration)
+        }
+        
+        for animator in runningAnimations {
+            animator.pauseAnimation() //set speed to 0, which makes them interactable
+            animationProgressWhenInterrupted = animator.fractionComplete
+        }
+    }
+    
+    func updateInteractiveTransition(fractionCompleted: CGFloat) {
+        //update fraction complete for all animations
+        for animator in runningAnimations {
+            //moving finger up or down - this keeps all of our animations in the same state while we move across the screen
+            animator.fractionComplete = fractionCompleted + animationProgressWhenInterrupted
+        }
+    }
+    
+    func continueInteractiveTransition() {
+        //setting params to 0 means the property animator uses the remaining time in our animations
+        for animator in runningAnimations {
+            animator.continueAnimation(withTimingParameters: nil, durationFactor: 0)
+        }
+    }
    
 
 }
